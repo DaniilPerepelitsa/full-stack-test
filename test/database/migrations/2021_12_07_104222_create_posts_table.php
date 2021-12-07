697626ec7be2fccb423536d3b66346fb3f0455c6
daniil perepelytsia <officialdvalica@gmail.com>
2021-12-07 17:53:12 +0200

    update posts migration and add migration for creating fulltext index for posts table

--- test/database/migrations/2021_12_07_104222_create_posts_table.php
+++ test/database/migrations/2021_12_07_104222_create_posts_table.php
@@ -17,7 +17,7 @@ class CreatePostsTable extends Migration
             $table->id();
             $table->string('title');
             $table->string('slug');
-            $table->string('content');
+            $table->longText('content');
             $table->string('media');
             $table->timestamps();
         });
